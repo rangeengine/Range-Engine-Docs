458f84685903b655c7df9372e339c0899d7a9a15
RafaelTavares <rafaelgt34@gmail.com>
2022-08-28 19:15:20 -0300

    Easy Build - update gitignore

--- make.bat
+++ make.bat
@@ -8,7 +8,7 @@ if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
 set SOURCEDIR=source
-set BUILDDIR=build
+set BUILDDIR=RangeDoc_Build
 set SPHINXPROJ=UPBGEManual
 
 if "%1" == "" goto help
